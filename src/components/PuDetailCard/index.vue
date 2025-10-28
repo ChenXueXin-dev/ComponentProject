3c0244d8cb04438aeec8e7efe5c1de6ea664dc79
DESKTOP-NTRMANG\Mayn <xuexin@keyfil.com>
2025-10-28 17:37:25 +0800

    feat:详情基础实现

---
+++ src/components/PuDetailCard/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <el-card class="card-wrapper" shadow="never">
+      <!-- <template #header>
+        <div>header</div>
+      </template>
+      <template #extra>
+        <div>extra</div>
+      </template>
+      <template #footer>
+        <div>footer</div>
+      </template> -->
+      <div class="toptitle" v-if="title">
+        <div class="titleLine"></div>
+        <span>
+          {{ title }}
+        </span>
+      </div>
+      <div class="columnContent">
+        <el-row
+          class="columnrowitem"
+          v-for="(column, rowIndex) in detailColumns"
+          :key="rowIndex"
+        >
+          <el-col
+            v-for="(item, colindex) in column.columns"
+            :span="item.span || 6"
+            :key="colindex"
+          >
+            <slot
+              name="bodyCell"
+              :row="datasource"
+              :column="item"
+              :index="colindex"
+            >
+              {{ item.label }}：
+              <span v-if="item.customRender">
+                {{ item.customRender(data, item) }}
+              </span>
+              <span v-else>{{ data[item.value] }}</span>
+            </slot>
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+defineOptions({
+  name: "PuDetailCard",
+});
+
+const props = defineProps({
+  title: { type: String, default: "" },
+  columns: { type: Array, default: () => [] },
+  data: { type: Object, default: () => ({}) },
+});
+
+const jsonStringify = (col, row) => {};
+
+const detailColumns = computed(() => {
+  return props.columns;
+});
+
+const datasource = computed(() => {
+  return props.data;
+});
+</script>
+
+<style scoped>
+.card-wrapper {
+  width: 100%;
+}
+:deep(.el-card__header) {
+  padding: 15px !important;
+  border-bottom: none !important;
+}
+
+:deep(.el-card__body) {
+  padding: 15px !important; /* 去除主体内边距 */
+}
+
+.toptitle {
+  display: flex;
+  font-size: 16px;
+  font-weight: 800 !important;
+  height: 28px;
+  align-items: center;
+  gap: 10px;
+  padding: 0 0 10px 0;
+}
+
+.titleLine {
+  height: 20px;
+  width: 3px;
+  border-radius: 10px;
+  background: #439fff;
+}
+
+.columnContent {
+  font-size: 12px;
+}
+
+.columnrowitem {
+  padding: 8px 0;
+}
+</style>
--- src/views/Home/detail/index.vue
+++ src/views/Home/detail/index.vue
@@ -1,7 +1,65 @@
 <template>
-  <div>详情</div>
+  <div>
+    <PuDetailCard
+      :title="'基础信息'"
+      :columns="baseColumns"
+      :data="datasource"
+    ></PuDetailCard>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const baseColumns = computed(() => {
+  return [
+    {
+      columns: [
+        { label: "姓名1", value: "name1" },
+        { label: "年龄1", value: "age1" },
+        { label: "地址1", value: "address1" },
+        {
+          label: "操作1",
+          value: "action1",
+          customRender: (row, col) => {
+            return row.name1 + "/" + col.label;
+          },
+        },
+      ],
+    },
+    {
+      columns: [
+        { label: "姓名2", value: "name2" },
+        { label: "年龄2", value: "age2" },
+        { label: "地址2", value: "address2" },
+        { label: "操作2", value: "action2" },
+      ],
+    },
+    {
+      columns: [
+        { label: "姓名3", value: "name3" },
+        { label: "年龄3", value: "age3" },
+        { label: "地址3", value: "address3" },
+        { label: "操作3", value: "action3" },
+      ],
+    },
+  ];
+});
+
+const datasource = ref({
+  name1: "张三",
+  age1: 18,
+  address1: "上海",
+  action1: "编辑",
+  name2: "张三",
+  age2: 18,
+  address2: "上海",
+  action2: "编辑",
+  name3: "张三",
+  age3: 18,
+  address3: "上海",
+  action3: "编辑",
+});
+</script>
 
 <style scoped></style>
